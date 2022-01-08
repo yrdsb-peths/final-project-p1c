05c349707a4699ceb7c549fecf93489133d6b614
F0lkl0re <51024076+F0lkl0re@users.noreply.github.com>
2022-01-07 21:48:32 -0500

    Create Player Class and add movement

--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,8 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        Player player = new Player();
+        addObject(player, 300, 200);
     }
 }
---
+++ Player.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player extends Actor
+{
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+       if (Greenfoot.isKeyDown("d"))
+      {
+          move(3);
+      }
+      else if (Greenfoot.isKeyDown("a"))
+      {
+          move(-3);
+      }
+      else if (Greenfoot.isKeyDown("w"))
+      {
+          setLocation(getX(), getY() - 3);
+      }
+      else if (Greenfoot.isKeyDown("s"))
+      {
+          setLocation(getX(), getY() + 3);
+      }
+    }
+}
