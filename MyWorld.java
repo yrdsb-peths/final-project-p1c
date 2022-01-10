b5a9afd17dfe1e89c7e88584a8ad90fc1f85c207
F0lkl0re <51024076+F0lkl0re@users.noreply.github.com>
2022-01-10 08:31:35 -0500

    Changed world size

--- MyWorld.java
+++ MyWorld.java
@@ -16,7 +16,7 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(800, 800, 1); 
+        super(800, 700, 1); 
         
         Player player = new Player();
         addObject(player, 400, 400);
