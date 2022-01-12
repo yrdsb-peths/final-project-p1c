1450c747471deaa8438e91788031199127437ba7
F0lkl0re <51024076+F0lkl0re@users.noreply.github.com>
2022-01-11 21:45:50 -0500

    Add level2 and 3 worlds

---
+++ LevelTwo.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LevelTwo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LevelTwo extends World
+{
+
+    /**
+     * Constructor for objects of class LevelTwo.
+     * 
+     */
+    public LevelTwo()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
