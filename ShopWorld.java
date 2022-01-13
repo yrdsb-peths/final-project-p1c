6406a1a927650349a61863107bb564110a7d9c61
Aninda S <349578690@gapps.yrdsb.ca>
2022-01-13 09:09:50 -0500

    shop created

---
+++ ShopWorld.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ShopWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ShopWorld extends World
+{
+    public static int money = 0;
+    /**
+     * Constructor for objects of class ShopWorld.
+     * 
+     */
+    public ShopWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
